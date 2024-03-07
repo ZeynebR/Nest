7c1854c389d01e9a0c8d8328ffeca745c3f9857f
ZeynebR <zeynabnr@code.edu.az>
2024-03-07 10:22:35 +0100

    product and category models added and migrated to database

---
+++ Nest/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Nest.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public double? Rating { get; set; }
+        public decimal SellPrice { get; set; } = default!;
+        public decimal? DiscountPrice { get; set; }
+        public List<ProductImage> ProductImages { get; set; } = null!;
+    }
+}
