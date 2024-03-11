1bb78a69345574c0107a9d7b7143bf099c64651e
ZeynebR <zeynabnr@code.edu.az>
2024-03-11 10:25:54 +0100

    area added

---
+++ Nest/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nest.Data;
+
+namespace Nest.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Nest/Data/AppDbContext.cs
+++ Nest/Data/AppDbContext.cs
@@ -5,6 +5,7 @@ namespace Nest.Data
 {
     public class AppDbContext: DbContext
     {
+       
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
 
         public DbSet<Product> Products { get; set; }
